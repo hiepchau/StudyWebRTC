120c45b5eff3a1e40e53e8332ac1cd50bef73ee7
hiepchau <ch.dhiep@gmail.com>
2023-03-13 14:04:25 +0700

    config file + add extensions

--- StudyWebRTC/AppDelegate.swift
+++ StudyWebRTC/AppDelegate.swift
@@ -7,30 +7,43 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    internal var window: UIWindow?
+    private let config = Config.default
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = self.buildMainViewController()
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    private func buildMainViewController() -> UIViewController {
+        
+        let webRTCClient = WebRTCClient(iceServers: self.config.webRTCIceServers)
+        let signalClient = self.buildSignalingClient()
+        let mainViewController = MainViewController(signalClient: signalClient, webRTCClient: webRTCClient)
+        let navViewController = UINavigationController(rootViewController: mainViewController)
+        navViewController.navigationBar.prefersLargeTitles = true
+        return navViewController
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    private func buildSignalingClient() -> SignalingClient {
+        
+        // iOS 13 has native websocket support. For iOS 12 or lower we will use 3rd party library.
+        let webSocketProvider: WebSocketProvider
+        
+        if #available(iOS 13.0, *) {
+            webSocketProvider = NativeWebSocket(url: self.config.signalingServerUrl)
+        } else {
+            webSocketProvider = StarscreamWebSocket(url: self.config.signalingServerUrl)
+        }
+        
+        return SignalingClient(webSocket: webSocketProvider)
     }
-
-
 }
 
---
+++ StudyWebRTC/Extensions/IceCandidate.swift
@@ -0,0 +1,26 @@
+//
+//  IceCandidate.swift
+//  StudyWebRTC
+//
+//  Created by Châu Hiệp on 13/03/2023.
+//
+
+import Foundation
+import WebRTC
+
+/// This struct is a swift wrapper over `RTCIceCandidate` for easy encode and decode
+struct IceCandidate: Codable {
+    let sdp: String
+    let sdpMLineIndex: Int32
+    let sdpMid: String?
+    
+    init(from iceCandidate: RTCIceCandidate) {
+        self.sdpMLineIndex = iceCandidate.sdpMLineIndex
+        self.sdpMid = iceCandidate.sdpMid
+        self.sdp = iceCandidate.sdp
+    }
+    
+    var rtcIceCandidate: RTCIceCandidate {
+        return RTCIceCandidate(sdp: self.sdp, sdpMLineIndex: self.sdpMLineIndex, sdpMid: self.sdpMid)
+    }
+}
